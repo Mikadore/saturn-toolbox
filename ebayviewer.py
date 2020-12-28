b0e899e9b557fd47f5f5cdf560303107dc38c926
akavenus <71657809+akavenus@users.noreply.github.com>
2020-12-28 15:51:33 -0500

    50

--- ebayviewer.py
+++ ebayviewer.py
@@ -14,13 +14,12 @@ def start():
     def view(use_proxies: bool, proxies=None): 
         listing_url = input("What is your listing link? ")
         view_number = int(input("How many views would you like to send? "))
-        print("Sending views")
-        print("Do not close the application")
+
 
     
         for i in range(view_number):
             if use_proxies:
-                headers = Headers(os="mac", headers=True).generate()
+                headers = Headers(os="win", headers=True).generate()
                 r = requests.get(listing_url, proxies=proxies, headers=headers)
                 if r.status_code == 200:
                     print('View sent')
@@ -58,10 +57,12 @@ def start():
         x_mode = input("Which mode would you like to use? (1/2) ")
 
         if x_mode == '1':
+            print('Starting')
             view(use_proxies=False)
             break
         elif x_mode == '2':  
             proxy_file = input("What is the PATH of your proxy file? ")
+            print('Starting')
             proxy = set()
 
             with open(proxy_file, "r") as f:
@@ -71,7 +72,7 @@ def start():
                 'http': 'http://' + random.choice(list(proxy))
             }
 
-        
+            
             view(use_proxies=True, proxies=proxies)
             break
         else:
